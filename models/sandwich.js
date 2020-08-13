83f65928c0ed864498287f5880414508054700d4
Christian Shaw <cshaw00048@gmail.com>
2020-08-13 10:42:13 -0500

    db and javascript updates

--- models/sandwich.js
+++ models/sandwich.js
@@ -1,24 +1,22 @@
-var orm = require("../config/orm.js");
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
+var orm = require('../config/orm.js');
+
+var sandwich = {
+	all: function(cb) {
+		orm.all('sandwiches', function(res){
+			cb(res);
+		});
+	},
+	create: function(cols, vals, cb) {
+		orm.create('sandwiches', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb){
+		orm.update('sandwiches', objColVals, condition, function(res){
+			cb(res);
+		});
+	}
 };
 
-module.exports = burger;
+module.exports = sandwich;
